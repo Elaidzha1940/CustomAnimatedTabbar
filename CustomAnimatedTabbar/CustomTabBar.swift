2a859c4bf193936ca1abcc286e991077a4021b0f
Elaidzha1940 <eli080293@gmail.com>
2023-09-09 20:37:30 +0300

    added CustomAnimatedTabbar

---
+++ CustomAnimatedTabbar/CustomTabBar.swift
@@ -0,0 +1,27 @@
+//  /*
+//
+//  Project: CustomAnimatedTabbar
+//  File: CustomTabBar.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 09.09.2023
+//
+//  Status: in progress | Decorated
+//
+//  */
+
+import SwiftUI
+
+struct CustomTabBar: View {
+    var body: some View {
+        
+        VStack {
+            
+        }
+    }
+}
+
+struct CustomTabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomTabBar()
+    }
+}
