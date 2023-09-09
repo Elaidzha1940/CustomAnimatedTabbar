4ab1ee19d3fca4018df4a3a4f6156c47f717e785
Elaidzha1940 <eli080293@gmail.com>
2023-09-09 20:31:45 +0300

    CustomAnimatedTabbar

---
+++ CustomAnimatedTabbar/CustomAnimatedTabbar/ContentView.swift
@@ -0,0 +1,27 @@
+//  /*
+//
+//  Project: CustomAnimatedTabbar
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 09.09.2023
+//
+//  Status: in progress | Decorated
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+        
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomAnimatedTabbar/CustomAnimatedTabbar/CustomAnimatedTabbarApp.swift
@@ -0,0 +1,21 @@
+//  /*
+//
+//  Project: CustomAnimatedTabbar
+//  File: CustomAnimatedTabbarApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 09.09.2023
+//
+//  Status: in progress | Decorated
+//
+//  */
+
+import SwiftUI
+
+@main
+struct CustomAnimatedTabbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
