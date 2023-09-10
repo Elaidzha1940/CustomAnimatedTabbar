d6a4e11167e75ca7f5608776519dfce0a21952eb
Elaidzha1940 <eli080293@gmail.com>
2023-09-10 16:30:09 +0300

    added Tabbar cases into CustomAnimatedTabbar

--- CustomAnimatedTabbar/CustomTabBar.swift
+++ CustomAnimatedTabbar/CustomTabBar.swift
@@ -11,7 +11,21 @@
 
 import SwiftUI
 
+enum Tab: String, CaseIterable {
+    
+    case house
+    case message
+    case person
+    case leaf
+    case gearshape
+}
+
 struct CustomTabBar: View {
+    @Binding var selectedTab: Tab
+    private var fillImage: String {
+        selectedTab.rawValue + ".fill" 
+    }
+    
     var body: some View {
         
         VStack {
@@ -22,6 +36,6 @@ struct CustomTabBar: View {
 
 struct CustomTabBar_Previews: PreviewProvider {
     static var previews: some View {
-        CustomTabBar()
+        CustomTabBar(selectedTab: .constant(.house))
     }
 }
