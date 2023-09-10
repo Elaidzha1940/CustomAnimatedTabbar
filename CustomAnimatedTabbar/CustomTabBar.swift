439e47c15a4bb9c0b85d7ae941d76204fb963659
Elaidzha1940 <eli080293@gmail.com>
2023-09-10 16:45:50 +0300

    updated onTapGesture withAnimation

--- CustomAnimatedTabbar/CustomTabBar.swift
+++ CustomAnimatedTabbar/CustomTabBar.swift
@@ -23,13 +23,32 @@ enum Tab: String, CaseIterable {
 struct CustomTabBar: View {
     @Binding var selectedTab: Tab
     private var fillImage: String {
-        selectedTab.rawValue + ".fill" 
+        selectedTab.rawValue + ".fill"
     }
     
     var body: some View {
         
         VStack {
-            
+            HStack {
+                ForEach(Tab.allCases, id: \.rawValue) { tab in
+                    Spacer()
+                    Image(systemName: selectedTab == tab ? fillImage : tab.rawValue)
+                        .scaleEffect(selectedTab == tab ? 1.25 : 1.0)
+                        .foregroundColor(selectedTab == tab ? .black : .gray)
+                        .font(.system(size: 25, weight: .bold))
+                        .onTapGesture {
+                            withAnimation(.easeIn(duration: 0.1)) {
+                                selectedTab = tab
+                            }
+                        }
+                    Spacer()
+                    
+                }
+            }
+            .frame(width: nil, height: 60)
+            .background(.thinMaterial)
+            .cornerRadius(15)
+            .padding()
         }
     }
 }
